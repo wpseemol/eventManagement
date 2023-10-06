506cabd9f9d2b1d745b5bdbcf7ea81455047a28e
wpseemol <wpseemol@gmail.com>
2023-10-06 23:04:14 +0600

    my project barlow font install set

---
+++ event-management/src/components/navMneus/NavMenus.jsx
@@ -0,0 +1,15 @@
+
+const NavMenus = () => {
+    return (
+        <div className="border-2 border-black m-10 relative">
+            <ul className="absolute w-96 h-40 bg-gray-700 top-0 left-0">
+                <li>this link 1</li>
+                <li>this link 1</li>
+                <li>this link 1</li>
+            </ul>
+            <p>this elements</p>
+        </div>
+    );
+};
+
+export default NavMenus;
--- event-management/src/root/Root.jsx
+++ event-management/src/root/Root.jsx
@@ -1,11 +1,18 @@
+import NavMenus from '../components/navMneus/NavMenus';
+
 const Root = () => {
     return (
         <>
-            <header className="myContainer text-2xl font-bold">
+            <header className="font-barlow text-2xl font-bold">
+                <nav>
+                    <NavMenus />
+                </nav>
                 <h2>this is header</h2>
             </header>
-            <main className="myContainer">this is Min section</main>
-            <footer className="myContainer">this is footer section</footer>
+            <main className="font-barlow myContainer">this is Min section</main>
+            <footer className="font-barlow myContainer">
+                this is footer section
+            </footer>
         </>
     );
 };
