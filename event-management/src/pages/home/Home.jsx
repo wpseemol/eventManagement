e5a81abae7202d8be0ae2aa61bc21f67ce2e7df0
wpseemol <wpseemol@gmail.com>
2023-10-08 00:24:01 +0600

    primaim section compleated withe responsive desigine

--- event-management/src/pages/home/Home.jsx
+++ event-management/src/pages/home/Home.jsx
@@ -1,8 +1,8 @@
 import { useLoaderData } from 'react-router-dom';
 
 const Home = () => {
-    const { experience, experienceColor } = useLoaderData();
-    console.log(experience);
+    const { premium, experience, experienceColor } = useLoaderData() || {};
+    console.log(premium[0]);
 
     return (
         <>
@@ -35,13 +35,42 @@ const Home = () => {
                                         <h2 className="text-2xl font-semibold mb-2">
                                             {data.title}
                                         </h2>
-                                        <p className="font-medium">
+                                        <p className="text-lg font-medium">
                                             {data.Description}
                                         </p>
                                     </div>
                                 </div>
                             );
                         })}
+                    </div>
+                </div>
+            </section>
+            {/* Premium section */}
+            <section className="myContainer py-24">
+                <div className="lg:flex gap-6 px-2 sm:px-0">
+                    <div className="lg:w-1/2 sm:flex lg:block gap-2 lg:mb-0 mb-5">
+                        <div className="h-1/2 lg:w-fit sm:w-1/2">
+                            <h2 className="lg:text-5xl text-3xl font-semibold mb-5 ">
+                                {premium[0].title}
+                            </h2>
+                            <p className="text-lg font-medium xl:pr-12 sm:pb-0 pb-3">
+                                {premium[0].description}
+                            </p>
+                        </div>
+                        <div className="lg:h-1/2 lg:w-fit sm:w-1/2">
+                            <img
+                                src={premium[0].themaiUrl}
+                                alt="Premium Item 1"
+                                className="w-full h-full object-cover object-center"
+                            />
+                        </div>
+                    </div>
+                    <div className="lg:w-1/2">
+                        <img
+                            src={premium[0].imageUrl}
+                            alt="Premium Item Image"
+                            className="w-full h-full object-cover object-center"
+                        />
                     </div>
                 </div>
             </section>
